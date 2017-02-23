d3be3d2f00f200bb7296c38600675205f1677f20
Gustav Jorlöv <gustav.jorlov@kits.se>
2017-02-23 05:41:24 +0100

    Refactorings to enable reading of code

--- index.js
+++ index.js
@@ -1,104 +1,60 @@
 var mqtt = require('mqtt');
 var fs = require('fs');
 var stdin = process.openStdin();
+var config = require('./config.json');
 
-// arn:aws:iot:eu-west-1:849037323980:thing/GurraDesktop
-// https://a15dqupkleyho9.iot.eu-west-1.amazonaws.com/things/GurraDesktop/shadow
-// var client = mqtt.connect('mqtt://a15dqupkleyho9.iot.eu-west-1.amazonaws.com/things/GurraDesktop/shadow', {
-//   key: fs.readFileSync('./certs/GurraDesktop.public.pem').toString(),
-//   cert: fs.readFileSync('./certs/GurraDesktop.cert.pem').toString(),
-//   ca: [fs.readFileSync('./certs/rootCA.pem').toString()],
-//   rejectUnauthorized: false
-// });
+console.log(config);
+var IoTEndpoint = 'mqtt://'+config.IoTEndpoint;
+var thingName = config.thingName;
 
+var certs = {
+  key: fs.readFileSync(config.privatekey).toString(),
+  cert: fs.readFileSync(config.certificate).toString(),
+  ca: fs.readFileSync(config.rootCA).toString()
+};
 
-
-function connectToAWSMQTT(url, certs, callback){
-  var client = mqtt.connect(url, {
+function connectToAWSMQTT(url, certs){
+  return mqtt.connect(url, {
     key: certs.key,
     cert: certs.cert,
     ca: certs.ca,
     rejectUnauthorized: false
   });
-  console.log("client made");
-  callback(null, client);
+}
+function setupAndListenToConnection(IoTEndpoint, certs, connected){
+  var client = connectToAWSMQTT(IoTEndpoint, certs);
+  client.on('connect', function(){
+    connected(client);
+  });
+}
+function keyMapper(key) {
+  return key === 'w' ? 'up'
+       : key === 'd' ? 'right'
+       : key === 'a' ? 'left'
+       : key === 's' ? 'down'
+       : null;
+}
+function printMqttMessage(topic, message){
+  console.log(topic, ':', JSON.parse(message.toString()));
+}
+function updateThing(client, thing, payload){
+  client.publish('$aws/things/'+thing+'/shadow/update', JSON.stringify({
+    "state": {
+      "reported" : {
+        "direction" : payload
+      }
+    }
+  }));
 }
 
-
-
-
-// var certs = {
-//   key: fs.readFileSync('./certs/GurraDesktop.public.pem').toString(),
-//   cert: fs.readFileSync('./certs/GurraDesktop.cert.pem').toString(),
-//   ca: fs.readFileSync('./certs/rootCA.pem').toString()
-// };
-
-var certs = {
-  key: fs.readFileSync('./kitscerts/GurrasPi-private.pem.key').toString(),
-  cert: fs.readFileSync('./kitscerts/GurrasPi-certificate.pem.crt').toString(),
-  ca: fs.readFileSync('./kitscerts/VeriSign-Class_3-Public-Primary-Certification-Authority-G5.pem').toString()
-};
-
-// connectToAWSMQTT('mqtt://a15dqupkleyho9.iot.eu-west-1.amazonaws.com/things/GurraDesktop/shadow', certs, function(err, client){
-// connectToAWSMQTT('mqtt://axl1uxnehfk9n.iot.eu-west-1.amazonaws.com/things/GurrasPi/shadow', certs, function(err, client){
-connectToAWSMQTT('mqtt://axl1uxnehfk9n.iot.eu-west-1.amazonaws.com', certs, function(err, client){
-  console.log("listening to mqtt and keys");
-  stdin.addListener("data", function(d) {
-    var key = d.toString().trim();
-    var direction = null;
-    switch (key) {
-      case 'w':
-        direction = "up";
-        break;
-      case 'd':
-        direction = "right";
-        break;
-      case 'a':
-        direction = "left";
-        break;
-      case 's':
-        direction = "back";
-        break;
-      default:
-        direction = null;
-    }
-    const shadowUpdate = {"state": {"reported" : {"direction" : direction}}};
-    client.publish('$aws/things/GurrasPi/shadow/update', JSON.stringify(shadowUpdate), function(mqttErr, granted){
-      // console.log(mqttErr, granted);
-    });
+setupAndListenToConnection(IoTEndpoint, certs, function(client){
+  stdin.addListener("data", function(pressedKey) {
+    var direction = keyMapper(pressedKey.toString().trim());
+    updateThing(client, thingName, direction);
   });
 
-  client.on('connect', function () {
-    client.subscribe('direction');
-    client.subscribe('$aws/things/GurrasPi/shadow/update/accepted');
-  });
+  client.subscribe('direction');
+  client.subscribe('$aws/things/'+thingName+'/shadow/update/accepted');
 
-  client.on('message', function(topic, message){
-    console.log(topic);
-    console.log(JSON.parse(message.toString()));
-  });
-
-
-
-
-
-  // client.on('message', function (topic, message) {
-  //   console.log("topic", topic);
-  //   console.log("payload", message.toString()); // message is Buffer
-  //
-  //
-  //   if (message.toString() === 'update1') {
-  //     console.log(`client.publish('$aws/things/GurraDesktop/shadow/update', JSON.toString(shadowUpdate));`);
-  //     client.publish('$aws/things/GurraDesktop/shadow/update', JSON.toString(shadowUpdate));
-  //     client.publish('$aws/things/GurraDesktop/shadow/update', shadowUpdate);
-  //   } else if (message.toString() === 'update2') {
-  //     client.publish('presence', '2');
-  //   }
-  //
-  //   // client.end();
-  // });
-
-
-
-
+  client.on('message', printMqttMessage);
 });
