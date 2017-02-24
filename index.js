d1837c0655dc35ed19a9d9fcbec65fa3ac53164b
Gustav Jorlöv <gustav.jorlov@gmail.com>
2017-02-24 10:19:04 +0100

    removed key controller options

--- index.js
+++ index.js
@@ -1,11 +1,10 @@
 var mqtt = require('mqtt');
 var fs = require('fs');
-var stdin = process.openStdin();
 var config = require('./config.json');
-
 var IoTEndpoint = 'mqtt://'+config.IoTEndpoint;
 var thingName = config.thingName;
 var button = require('./buttons.js');
+var speed = 50;
 
 var certs = {
   key: fs.readFileSync(config.privatekey).toString(),
@@ -27,13 +26,6 @@ function setupAndListenToConnection(IoTEndpoint, certs, connected){
     connected(client);
   });
 }
-function keyMapper(key) {
-  return key === 'w' ? 1
-       : key === 'd' ? 90
-       : key === 'a' ? 270
-       : key === 's' ? 180
-       : null;
-}
 function printMqttMessage(topic, message){
   console.log(topic, ':', JSON.parse(message.toString()));
 }
@@ -49,26 +41,21 @@ function updateThing(client, thing, payload){
   }));
 }
 
+function calculateDirection(buttonValue, directionState){
+	return buttonValue === 1 ? ((directionState + 90) >= 360 ? 2 : directionState + 90) : directionState;
+}
+
 setupAndListenToConnection(IoTEndpoint, certs, function(client){
   var directionState = 2;
-  stdin.addListener("data", function(pressedKey) {
-    var direction = keyMapper(pressedKey.toString().trim());
-    updateThing(client, thingName, direction);
-  });
-  button.stream.on('changed', function(value){
-	console.log(value);
-	directionState = value === 1 ? directionState + 90 : directionState;
-	directionState = directionState >= 360 ? 2 : directionState;
-	// var bb8Payload = value === 1 ? {direction: directionState, speed: 20, stop: 0} : {direction: directionState, stop: 1};
+  button.stream.on('changed', function(buttonValue){
+	directionState = calculateDirection(buttonValue, directionState)
     updateThing(client, thingName, {
 	  direction: directionState,
-	  speed: value === 1 ? 20 : 0,
-	  color: value === 1 ? "#74c689" : "#046e1f"
+	  speed: buttonValue === 1 ? speed : 0,
+	  color: buttonValue === 1 ? "#74c689" : "#046e1f"
 	});
   });
 
-  client.subscribe('direction');
   client.subscribe('$aws/things/'+thingName+'/shadow/update/accepted');
-
   client.on('message', printMqttMessage);
 });
